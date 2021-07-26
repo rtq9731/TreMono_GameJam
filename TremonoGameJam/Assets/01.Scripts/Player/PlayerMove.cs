e0b62266f0963098561ffae305c60584689bb208
Firstund <yjw040608@gmail.com>
2021-07-26 09:25:24 +0900

    PlayerMove관련 작업  푸쉬

--- TremonoGameJam/Assets/01.Scripts/Player/PlayerMove.cs
+++ TremonoGameJam/Assets/01.Scripts/Player/PlayerMove.cs
@@ -5,27 +5,46 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     private PlayerInput playerInput = null;
+    private PlayerStat playerStat = null;
+
+    private Rigidbody2D rigid = null;
     private SpriteRenderer spriteRenderer = null;
 
     private float XMove = 0f;
 
     void Start()
     {
-        playerInput = GetComponent<PlayerInput>();    
-        
+        playerInput = GetComponent<PlayerInput>();
+        playerStat = GetComponent<PlayerStat>();
+
         spriteRenderer = GetComponent<SpriteRenderer>();
+        rigid = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
 
     }
-    void FixedUpdatq()
+    void FixedUpdate()
     {
-        
+        XMove = playerInput.XMove;
+
+        LRCheck();
+        Move();
     }
     private void Move()
     {
-        
+        rigid.velocity = new Vector2(XMove * playerStat.speed, rigid.velocity.y);
+    }
+    private void LRCheck()
+    {
+        if (XMove < 0f)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
     }
 }
