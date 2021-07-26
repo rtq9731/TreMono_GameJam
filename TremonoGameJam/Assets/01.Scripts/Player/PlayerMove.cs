7d6d4b91e832fcb3b634c79ad191621dee8803f6
Firstund <yjw040608@gmail.com>
2021-07-26 09:14:53 +0900

    PlayerMove관련 코딩중

--- TremonoGameJam/Assets/01.Scripts/Player/PlayerMove.cs
+++ TremonoGameJam/Assets/01.Scripts/Player/PlayerMove.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     private PlayerInput playerInput = null;
+    private SpriteRenderer spriteRenderer = null;
+
+    private float XMove = 0f;
 
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();    
+        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-                
+
+    }
+    void FixedUpdatq()
+    {
+        
+    }
+    private void Move()
+    {
+        
     }
 }
