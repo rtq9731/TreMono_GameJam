bdcc4b5b97bd803b679b0a800651066148706c88
Firstund <yjw040608@gmail.com>
2021-07-26 12:16:19 +0900

    스탯관련 수정

--- TremonoGameJam/Assets/01.Scripts/MainScene/Player/PlayerStat.cs
+++ TremonoGameJam/Assets/01.Scripts/MainScene/Player/PlayerStat.cs
@@ -6,26 +6,20 @@ public class PlayerStat : MonoBehaviour
 {
     [Header("Player스탯 관련")]
     [SerializeField]
-    private float _hp = 3f;
-    public float hp
+    private int _hp = 3;
+    public int hp
     {
         get { return _hp; }
         set { _hp = value; }
     }
     [SerializeField]
-    private float _ap = 3f;
-    public float ap
+    private int _ap = 3;
+    public int ap
     {
         get { return _ap; }
         set { _ap = value; }
     }
-    [SerializeField]
-    private float _dp = 3f;
-    public float dp
-    {
-        get { return _dp; }
-        set { _dp = value; }
-    }
+
     [SerializeField]
     private float _speed = 1f;
     public float speed
