46415d53f4b08442cd58da38ce86087c9e07128a
Firstund <yjw040608@gmail.com>
2021-07-26 09:41:31 +0900

    MyGizmos스크립트 추가

---
+++ TremonoGameJam/Assets/01.Scripts/Gizmos/MyGizmos.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGizmos : MonoBehaviour
+{
+    [Header("어떤것에 관한 Gizmos인가")]
+    [SerializeField]
+    private string what = "";
+
+    [SerializeField]
+    private float radius = 1f;
+    [SerializeField]
+    private Color color;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = color;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
