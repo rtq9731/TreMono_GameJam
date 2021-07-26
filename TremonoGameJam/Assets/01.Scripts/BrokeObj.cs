08c1e7b050c9c177986fd17695a150028788cefd
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-07-26 22:29:57 +0900

    깨지는 블록 왼쪽

--- TremonoGameJam/Assets/01.Scripts/BrokeObj.cs
+++ TremonoGameJam/Assets/01.Scripts/BrokeObj.cs
@@ -13,7 +13,7 @@ public class BrokeObj : MonoBehaviour
 
     [SerializeField] brokeObjPos whereItis;
     [SerializeField] BrokeObj[] linkedObjs;
-    [SerializeField] bool isBroke;
+    [SerializeField] public bool isBroke;
 
     private void Update()
     {
@@ -32,12 +32,12 @@ public class BrokeObj : MonoBehaviour
                 Destroy(temp, 2f);
                 break;
             case brokeObjPos.Center:
-                temp = Instantiate(StageManager.Instance.brokeObjsPrefab[0], gameObject.transform.position, Quaternion.identity);
+                temp = Instantiate(StageManager.Instance.brokeObjsPrefab[1], gameObject.transform.position, Quaternion.identity);
                 Destroy(this.gameObject);
                 Destroy(temp, 2f);
                 break;
             case brokeObjPos.Left:
-                temp = Instantiate(StageManager.Instance.brokeObjsPrefab[0], gameObject.transform.position, Quaternion.identity);
+                temp = Instantiate(StageManager.Instance.brokeObjsPrefab[2], gameObject.transform.position, Quaternion.identity);
                 Destroy(this.gameObject);
                 Destroy(temp, 2f);
                 break;
@@ -47,7 +47,7 @@ public class BrokeObj : MonoBehaviour
 
         for (int i = 0; i < linkedObjs.Length; i++)
         {
-            linkedObjs[i].Break();
+            linkedObjs[i].isBroke = true;
         }
     }
 
