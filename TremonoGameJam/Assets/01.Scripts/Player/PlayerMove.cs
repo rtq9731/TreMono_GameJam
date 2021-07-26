269cd370c0001a76cfb101480c6cc88cffc18157
Firstund <yjw040608@gmail.com>
2021-07-26 09:09:27 +0900

    PlayerInput 푸쉬, PlayerMove 추가

---
+++ TremonoGameJam/Assets/01.Scripts/Player/PlayerMove.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private PlayerInput playerInput = null;
+
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();    
+    }
+
+    void Update()
+    {
+                
+    }
+}
