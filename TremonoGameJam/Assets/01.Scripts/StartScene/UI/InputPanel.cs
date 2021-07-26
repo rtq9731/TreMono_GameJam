dbd8b7e2e4bc9b35321f19c0c17ed5aeecc777e3
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-07-26 19:33:17 +0900

    사운드 / UI 이미지 추가

---
+++ TremonoGameJam/Assets/01.Scripts/StartScene/UI/InputPanel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputPanel : MonoBehaviour
+{
+    [SerializeField] public Button btnOK;
+    [SerializeField] public Button btnCancel;
+    [SerializeField] public Text msg;
+}
--- TremonoGameJam/Assets/01.Scripts/StartScene/UI/StageSelectMenu.cs
+++ TremonoGameJam/Assets/01.Scripts/StartScene/UI/StageSelectMenu.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class StageSelectMenu : MonoBehaviour
 {
     [SerializeField] Button[] selectBtns;
+    [SerializeField] InputPanel inputPanel;
+
+    bool bCanCallInputPanel = true;
 
     private void Start()
     {
@@ -26,6 +30,22 @@ public class StageSelectMenu : MonoBehaviour
 
     void ChangeScene(int stageNum)
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene($"Stage {stageNum}");
+        if (!bCanCallInputPanel)
+            return;
+
+        bCanCallInputPanel = false;
+        inputPanel.gameObject.SetActive(true);
+        inputPanel.msg.text = $"스테이지 {stageNum + 1}\n입장하시겠습니까?";
+        inputPanel.GetComponent<RectTransform>().DOAnchorPosY(0, 0.25f).OnComplete(() =>
+        {
+            inputPanel.btnOK.onClick.AddListener(() => UnityEngine.SceneManagement.SceneManager.LoadScene($"Stage {stageNum}"));
+            inputPanel.btnCancel.onClick.AddListener(() => inputPanel.GetComponent<RectTransform>().DOAnchorPosY(1000, 0.25f).SetEase(Ease.InBack).OnComplete(() =>
+            {
+                inputPanel.gameObject.SetActive(false);
+                bCanCallInputPanel = true;
+                inputPanel.btnCancel.onClick.RemoveAllListeners();
+                inputPanel.btnOK.onClick.RemoveAllListeners();
+            }));
+        });
     }
 }
